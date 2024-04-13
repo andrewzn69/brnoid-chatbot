a66c4a2a2a81b11992b9aa3e9a00d212e9678c79
andrewzn <ondrejzeman23@gmail.com>
2024-04-14 00:21:09 +0200

    feat(chatbot): add avatar

---
+++ components/chatbot/Avatar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from '../../styles/chatbot/Avatar.module.css';
+
+const Avatar: React.FC = () => {
+	return (
+		<div className={styles['ADE']}>ADE</div>
+	);
+}
+
+export default Avatar;
