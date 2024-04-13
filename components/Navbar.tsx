dfaf0f5496f4021dda22d70b313fd090914cc2d9
andrewzn <ondrejzeman23@gmail.com>
2024-04-13 12:38:52 +0200

    fix(navbar): fixed logo not being centered

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,17 @@
 import React from 'react'
 import styles from '../styles/navbar/Navbar.module.scss'
 import Logo from './navbar/Logo'
-// import LeftSide from './navbar/LeftSide'
+import LeftSide from './navbar/LeftSide'
 // import RightSide from './navbar/RightSide'
 
 const Navbar: React.FC = () => {
 	return (
 		<nav className={styles['navbar-container']}>
-			<Logo />
-			{/* <LeftSide /> */
-			/* <RightSide /> */}
+			<div className={styles['left-side']}>
+				<Logo />
+				<LeftSide />
+			</div>
+			{/* <RightSide /> */}
 		</nav>
 	)
 }
