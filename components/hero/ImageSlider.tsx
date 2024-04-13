34829c0b7ce8df6bc15c15f9564e9efe9710c676
andrewzn <ondrejzeman23@gmail.com>
2024-04-13 20:26:09 +0200

    perf(PIFDJDS LKJ): kjdsLKDS JFLKDS JA SE ZABI JU JA SE ZABIUJ
    AJ

--- components/hero/ImageSlider.tsx
+++ components/hero/ImageSlider.tsx
@@ -1,17 +1,54 @@
-import React, { useState, useEffect } from 'react'; import { imageData } from '../../data/imageData';
+import React, { useState, useEffect } from 'react';
+import { imageData as originalImageData } from '../../data/imageData';
+import styles from '../../styles/hero/ImageSlider.module.scss';
 
 const ImageSlider: React.FC = () => {
+	const imageData = [...originalImageData, originalImageData[0]]; // Add the first image at the end
 	const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
 	useEffect(() => {
 		const timer = setInterval(() => {
-			setCurrentImageIndex((currentImageIndex + 1) % imageData.length);
+			setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageData.length);
 		}, 5000);
 		return () => clearInterval(timer);
-	}, [currentImageIndex]);
+	}, []);
+
+	const percentagePerImage = 100 / imageData.length;
+	const offset = percentagePerImage * currentImageIndex;
+
+	// Generate keyframes
+	let keyframes = '';
+	for (let i = 0; i < imageData.length; i++) {
+		const startPercentage = i * percentagePerImage;
+		const endPercentage = (i + 1) * percentagePerImage - 12; // Subtract 13% to pause for 8 seconds
+		const pausePercentage = endPercentage + 12; // Add 13% to pause for 8 seconds
+		keyframes += `
+      ${startPercentage}% { transform: translateX(-${i * 100}%); }
+      ${endPercentage}% { transform: translateX(-${i * 100}%); }
+      ${pausePercentage}%, ${pausePercentage + percentagePerImage}% { transform: translateX(-${(i + 1) * 100}%); }
+    `;
+	}
 
 	return (
-		<img src={imageData[currentImageIndex].src} alt={imageData[currentImageIndex].alt} />
+		<div className={styles.slider}>
+			<style>
+				{`
+          @keyframes slide {
+            ${keyframes}
+          }
+        `}
+			</style>
+			<div className={styles.imageRow} style={{ animation: 'slide 200s infinite' }}>
+				{imageData.map((image, index) => (
+					<img
+						key={index}
+						src={image.src}
+						alt={image.alt}
+						className={styles.image}
+					/>
+				))}
+			</div>
+		</div>
 	);
 };
 
