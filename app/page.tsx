ba5fc3187852e5330a75d23ca3de1d69546e6afd
andrewzn <ondrejzeman23@gmail.com>
2024-04-13 12:34:38 +0200

    fix(globals.scss): fixed globals being module

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from '../components/Navbar';
 // import Hero from '../components/Hero';
 // import Main from '../components/Main';
 // import Footer from '../components/Footer';
-// '../styles/globals.module.css';
+import '../styles/globals.scss';
 
 export default function HomePage() {
 	return (
