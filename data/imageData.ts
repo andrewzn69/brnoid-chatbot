dbb712be445c2b11ffb092a9a531d702e711d5c9
andrewzn <ondrejzeman23@gmail.com>
2024-04-13 14:47:26 +0200

    feat(imagedata): add imagedata
    contains info about images and images themselves

---
+++ data/imageData.ts
@@ -0,0 +1,72 @@
+export const imageData = [
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/family_vouchers.jpg',
+		alt: 'Family Vouchers',
+		info: 'Modul Rodičovské vouchery'
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/tic2.jpg',
+		alt: 'TIC2',
+		info: 'Modul Turistická karta BRNOPAS',
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/kjm.jpg',
+		alt: 'kjm',
+		info: 'Modul Knihovna Jiřího Mahena',
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/zoo.jpg',
+		alt: 'zoo',
+		info: 'Modul Zoo',
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/sport.jpg',
+		alt: 'riviera',
+		info: 'Modul Sport',
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/observatory.jpg',
+		alt: 'observatory',
+		info: 'Modul Hvězdárna',
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/muzeum_mb2.jpg',
+		alt: 'muzeum',
+		info: 'Modul Muzeum města Brna',
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/parking.svg',
+		alt: 'parking',
+		info: 'Modul Rezidentní parkování',
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/cemetery.jpg',
+		alt: 'cemetery',
+		info: 'Modul Městské hřbitovy',
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/paro23-cb.jpg',
+		alt: 'nevim',
+		info: 'Modul Dáme na vás',
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/vzmb.jpg',
+		alt: 'zelen',
+		info: 'Modul Veřejná zeleň města Brna',
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/eoc.jpg',
+		alt: 'mhd',
+		info: 'Modul MHD',
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/idsjmk.jpg',
+		alt: 'idsjmk',
+		info: 'Modul IDS JMK',
+	},
+	{
+		src: 'https://www.brnoid.cz/themes/brnoid/img/services-photos/waste.jpg',
+		alt: 'zlutavestatakycesta',
+		info: 'Modul Odpady',
+	},
+]
